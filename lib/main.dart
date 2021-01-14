009fc564e7f3bf80e90d2ca82d7b7440ffe10140
hexaludira <hexaludira@gmail.com>
2021-01-14 13:58:50 +0700

    debug

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,19 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Test search',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: ExamplePage(),
+    );
+  }
+}
+
 class ExamplePage extends StatefulWidget {
   @override
   _ExamplePageState createState() => _ExamplePageState();
@@ -18,7 +31,7 @@ class _ExamplePageState extends State<ExamplePage> {
   String _searchtext = "";
 
   //membuat list
-  List data = List();
+  List data_list = List();
 
   List filteredData = List();
 
@@ -26,35 +39,76 @@ class _ExamplePageState extends State<ExamplePage> {
 
   Widget _appBarTitle = Text('Contoh cari data');
 
+  //constructor
+  _ExamplePageState() {
+    _filter.addListener(() { 
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchtext = "";
+          filteredData = data_list;
+        });
+      } else {
+        setState(() {
+          _searchtext = _filter.text;
+        });
+      }
+    });
+  }
+
   @override
   void initState() {
+    this._getData();
     super.initState();
-    void _getData() async {
-      final response = await dio.get('https://reqres.in/api/users?page=2');
-      List tempList = List();
-      for (int i = 0; i < response.data['results'].length; i++) {
-        tempList.add(response.data['results'][i]);
-      }
-
-      setState(() {
-        data = tempList;
-        filteredData = data;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-        leading: IconButton(
-          icon: _iconSearch,
-          onPressed: _searchPressed(),
-        ),
-        title: _appBarTitle,
-        centerTitle: true,
-      );
     
   }
+
+  //widget utama
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body: Container(
+        child: _buildList(),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+    
+  }
+
+
+  //widget appbar
+  Widget _buildBar(BuildContext context){
+    return AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: IconButton(
+        icon: _iconSearch, 
+        onPressed: () {
+          _searchPressed();
+        },
+      ),
+    );
+  }
+
+  void _getData() async {
+    final response = await dio.get('https://swapi.dev/api/people');
+    if (response != null) {
+      print("data OK");
+    } else {
+      print ("can't get data"); 
+    }
+    List tempList = List();
+    for (int i = 0; i < response.data['results'].length; i++) {
+      tempList.add(response.data['results'][i]);
+    }
+
+    setState(() {
+      data_list = tempList;
+      filteredData = data_list;
+    });
+  }
+
+ 
 
   void _searchPressed() {
     setState(() {
@@ -70,9 +124,34 @@ class _ExamplePageState extends State<ExamplePage> {
       } else {
         this._iconSearch = Icon(Icons.search);
         this._appBarTitle = Text('Search data');
-        filteredData = data;
+        filteredData = data_list;
         _filter.clear();
       }
     });
+  }
+
+  Widget _buildList() {
+    if ((_searchtext.isNotEmpty)) {
+      List tempList = List();
+      for (int i = 0; i < filteredData.length; i++) {
+        //mengecek apakah search text sama dengan filtered data
+        if(filteredData[i]['name'].toLowerCase().contains(_searchtext.toLowerCase())) {
+          tempList.add(filteredData[i]);
+        }
+      }
+      filteredData = tempList;
+    }
+
+    return ListView.builder(
+      itemCount: data_list == null ? 0 : filteredData.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(filteredData[index]['data']),
+          onTap: () => print(filteredData[index]['name']),
+        );
+      },
+
+    );
+
   }
 }
