79a0a41b770650b659817c4c07a64db8b1628381
hexaludira <hexaludira@gmail.com>
2021-01-15 14:10:46 +0700

    Coba Search Bar Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,7 +31,8 @@ class _ExamplePageState extends State<ExamplePage> {
 
   String _searchtext = "";
 
-  //membuat list
+ 
+
   List data_list = List();
 
   List filteredData = List();
@@ -41,7 +43,7 @@ class _ExamplePageState extends State<ExamplePage> {
 
   //constructor
   _ExamplePageState() {
-    _filter.addListener(() { 
+    _filter.addListener(() {
       if (_filter.text.isEmpty) {
         setState(() {
           _searchtext = "";
@@ -57,9 +59,8 @@ class _ExamplePageState extends State<ExamplePage> {
 
   @override
   void initState() {
-    this._getData();
+    //this._getData();
     super.initState();
-    
   }
 
   //widget utama
@@ -67,22 +68,96 @@ class _ExamplePageState extends State<ExamplePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildBar(context),
-      body: Container(
-        child: _buildList(),
+      // body: Container(
+      //   child: _buildList(),
+      // ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          _buildList(),
+          //_buildFloatingSearchBar(),
+          _buildFloatingSearchAppBar(),
+        ],
       ),
       resizeToAvoidBottomInset: false,
     );
-    
   }
 
+  Widget _buildFloatingSearchAppBar(){
+    return FloatingSearchAppBar(
+      title: const Text('Title'),
+      transitionDuration: const Duration(milliseconds: 800),
+      color: Colors.greenAccent.shade100,
+      colorOnScroll: Colors.greenAccent.shade200,
+      body: ListView.separated(
+        padding: EdgeInsets.zero, 
+        itemCount: 100,
+        separatorBuilder: (context, index) => const Divider(),
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item $index'),
+          );
+        }, 
+        
+      ),
+
+    );
+  }
+
+  Widget _buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      maxWidth: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {},
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place), 
+            onPressed: (){}
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color,);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   //widget appbar
-  Widget _buildBar(BuildContext context){
+  Widget _buildBar(BuildContext context) {
     return AppBar(
       centerTitle: true,
       title: _appBarTitle,
       leading: IconButton(
-        icon: _iconSearch, 
+        icon: _iconSearch,
         onPressed: () {
           _searchPressed();
         },
@@ -95,7 +170,7 @@ class _ExamplePageState extends State<ExamplePage> {
     // if (response.statusCode == 200) {
     //   print("data OK");
     // } else {
-    //   print ("can't get data"); 
+    //   print ("can't get data");
     // }
     //print('WArning');
     List tempList = List();
@@ -108,8 +183,6 @@ class _ExamplePageState extends State<ExamplePage> {
       filteredData = data_list;
     });
   }
-
- 
 
   void _searchPressed() {
     setState(() {
@@ -136,7 +209,7 @@ class _ExamplePageState extends State<ExamplePage> {
       List tempList = new List();
       for (int i = 0; i < filteredData.length; i++) {
         //mengecek apakah search text sama dengan filtered data
-        if(filteredData[i]['name']..contains(_searchtext.toLowerCase())) {
+        if (filteredData[i]['name'].contains(_searchtext.toLowerCase())) {
           tempList.add(filteredData[i]);
         }
       }
@@ -151,8 +224,6 @@ class _ExamplePageState extends State<ExamplePage> {
           onTap: () => print(filteredData[index]['name']),
         );
       },
-
     );
-
   }
 }
